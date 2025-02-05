5e59694d120656fda0be207451a32dff851e3239
Martin MOTZ <martinmotzcontact@gmail.com>
2025-02-05 22:14:35 +0100

    Minor changes

--- alvesmusic.py
+++ alvesmusic.py
@@ -5,6 +5,7 @@ class AlvesMusic(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
+
         super().__init__(command_prefix="!", intents=intents)
 
         self.data = {}
